1b16ce0fc9843a24aac91774c860c627d282d92a
Joe <jakatsa5@gmail.com>
2024-04-15 22:16:08 +0300

    completed

--- server/app.py
+++ server/app.py
@@ -14,13 +14,62 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages',methods=['GET', 'POST'])
 def messages():
-    return ''
+   
+    # if to GET/POST
+    if request.method == 'GET':
+        
+     # get the messeges ordered by id in ascending order
+        messages = Message.query.order_by('created_at').all()
+    # GET- list to dict then make reponse
+        return make_response([message.to_dict() for message in messages],200 )
 
-@app.route('/messages/<int:id>')
+    # Post- make dict, add, commit the return the make response with the 200
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(
+            body=data['body'],
+            username=data['username']
+        )
+        #   add
+        db.session.add(message)
+        # commit
+        db.session.commit()
+        #  return the make response  to_dict with the 200
+        return  make_response(message.to_dict(),  201,)
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    # get the messange by id
+    message = Message.query.filter_by(id=id).first()
+     #  if PATCH loop though the object , use setattr(message, attr, assign), add, commit 
+    if request.method == 'PATCH':
+        data = request.get_json()
+        for attr in data:
+            setattr(message, attr, data[attr])
+            
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(message.to_dict(),200 )
+   
+    # if DEl delete and commit
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        return make_response( {'deleted': True} , 200)
+    # return the response in both
+    pass
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
